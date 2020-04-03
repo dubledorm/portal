ebb0e8bb68fde662610dca782a52d0b68d449e40
Aleksey Aleshin <dubledorm@yandex.ru>
2020-04-03 20:17:52 +0300

    Подключены стили и скрипты

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../corlate_assets/css .css
+//= link_directory ../corlate_assets/js .js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( js/jquery.js js/bootstrap.min.js js/jquery.prettyPhoto.js \
+ js/owl.carousel.min.js js/jquery.isotope.min.js js/main.js)
