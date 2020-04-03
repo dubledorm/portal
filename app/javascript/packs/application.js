c3722bad454d6b08d3a7285f9f5cb1cf177e7720
Aleksey Aleshin <dubledorm@yandex.ru>
2020-04-03 17:18:19 +0300

    Отлажено верхнее меню

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+// require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -34,7 +34,7 @@ import('../styles/libra/css/featurestab');
 import('../styles/libra/css/contact_form');
 import('../styles/libra/css/custom');
 
-import('../js/jquery/jquery');
+// import $ from 'jquery';
 
 
 var componentRequireContext = require.context("components", true);
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( libra/js/comment-reply.min.js libra/js/underscore.min.js
+libra/sliders/polaroid/js/jquery.polaroid.js libra/sliders/polaroid/js/jquery.transform-0.8.0.min.js
+libra/sliders/polaroid/js/jquery.preloader.js libra/js/jquery.colorbox-min.js
+libra/js/jquery.masonry.js libra/js/jquery.easing.js libra/js/hoverIntent.min.js libra/js/media-upload.min.js
+libra/js/jquery.clickout.min.js libra/js/responsive.js libra/js/mobilemenu.js libra/js/jquery.superfish.js
+libra/js/jquery.placeholder.js libra/js/contact.js libra/js/jquery.tipsy.js libra/js/jquery.cycle.min.js
+libra/js/shortcodes.js libra/js/jquery.custom.js libra/js/jquery/jquery.js )
