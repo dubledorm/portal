36b004af8ecbe4023a3e56cb2055584b33e88ac0
Aleksey Aleshin <dubledorm@yandex.ru>
2020-04-07 13:47:49 +0300

    Добавлены контроллеры Devise. Сделано перенаправление на user_path после редактирования профиля пользователя.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'users/registrations' }
   devise_scope :user do
     get '/users/auth/do_omniauth', to: 'omniauth_callbacks#do_omniauth', as: :auth_callback_do
     get '/users/auth/service_sign_up', to: 'omniauth_callbacks#service_sign_up_users', as: :service_sign_up_users
