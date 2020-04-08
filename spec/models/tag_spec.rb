2eb5fbc9494c4e41fe2844f8b4ab445e70f37b54
Aleksey Aleshin <dubledorm@yandex.ru>
2020-04-08 22:26:35 +0300

    tag scopy by_user

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -27,4 +27,23 @@ RSpec.describe Tag, type: :model do
     it { expect(FactoryGirl.build(:tag, name: tag.name, tag_type: tag.tag_type)).to be_invalid }
     it { expect(FactoryGirl.build(:tag, name: tag.name, tag_type: 'category')).to be_valid }
   end
+
+  describe 'scope' do
+    let!(:tag1) {FactoryGirl.create :tag}
+    let!(:tag2) {FactoryGirl.create :tag}
+    let!(:user) {FactoryGirl.create :user}
+    let!(:user1) {FactoryGirl.create :user}
+
+
+    before :each do
+      user.add_tag(tag2.name)
+      user1.add_tag('new_name')
+    end
+
+    it { expect(Tag.count).to eq(3) }
+    it { expect(Tag.by_user(user).count).to eq(1) }
+    it { expect(Tag.by_user(user).first).to eq(tag2) }
+    it { expect(Tag.by_user(user1).first.name).to eq('new_name') }
+
+  end
 end
