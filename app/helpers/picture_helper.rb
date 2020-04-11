192c6f4ed7356afd91e83ae15d445df909015ff6
Aleksey Aleshin <dubledorm@yandex.ru>
2020-04-11 12:39:54 +0300

    Галерея

--- app/helpers/picture_helper.rb
+++ app/helpers/picture_helper.rb
@@ -4,12 +4,16 @@ module PictureHelper
     image_tag picture.image, options  if picture.image.attached?
   end
 
-  def gallery_cover_small(gallery)
+  def cover_gallery_small(gallery)
     if gallery.image_for_cover.attached?
       background_image = url_for(gallery.image_for_cover.variant(resize_to_limit: [600, 600]))
     else
       background_image = asset_path('images/sidebar-g-1.png')
     end
-    content_tag(:div, class: :image_in_column_gallery, style: "background-image: url('#{ background_image }');" ) {}
+    content_tag(:div, class: :cover_gallery_small, style: "background-image: url('#{ background_image }');" ) {}
+  end
+
+  def cover_image(image)
+    content_tag(:div, class: :cover_image, style: "background-image: url('#{ url_for(image.variant(resize_to_limit: [600, 600])) }');" ) {}
   end
 end
