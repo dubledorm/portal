72692edccfdb75452f40c1e729ea7c9a94725364
Aleksey Aleshin <dubledorm@yandex.ru>
2020-04-12 23:10:29 +0300

    Реализовано открытие/закрытие редактируемого поля через JQuery

---
+++ app/assets/javascripts/block_content_form.js
@@ -0,0 +1,41 @@
+function toggle_block_content(e) {
+    var block_content = e.parents('.block-content');
+
+    if ( block_content[0] == undefined ) {
+        console.error('Coild not find the parent with class block-content');
+        return;
+    }
+
+
+    block_content_id = block_content.attr('id');
+
+    if ( block_content_id == undefined ) {
+        console.error('Element with class block-content should has an ID');
+        return;
+    }
+
+    var selector_str = ".block-hidden-form[data-params-block-id='" + block_content_id + "']";
+
+    block_content.find('.block-visible-data').toggle();
+    block_content.find(selector_str).toggle(300);
+}
+
+$(document).ready(function() {
+    $('.block-content .block-visible-data').on(
+        'click',
+        function(e) {
+            toggle_block_content($(this));
+        }
+    );
+
+    $('.block-content a.btn-cancel').on(
+        'click',
+        function(e) {
+           e.preventDefault();
+           toggle_block_content($(this));
+        }
+    );
+
+});
+
+//$(document).click(function(e){ console.log($(e.target).attr('id')) })
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( js/jquery.js js/bootstrap.min.js js/jquery.prettyPhoto.js \
- js/owl.carousel.min.js js/jquery.isotope.min.js js/main.js)
+ js/owl.carousel.min.js js/jquery.isotope.min.js js/main.js hello.js block_content_form.js)
