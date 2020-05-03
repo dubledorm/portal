97a2d344a2267ba8fd2ad8fd6e291990908db8c5
Aleksey Aleshin <dubledorm@yandex.ru>
2020-05-03 10:10:40 +0300

    gallery_decorator.small_cover_url

--- app/decorators/gallery_decorator.rb
+++ app/decorators/gallery_decorator.rb
@@ -5,6 +5,6 @@ class GalleryDecorator < Draper::Decorator
   def small_cover_url
     return rails_representation_url(object.image_for_cover.variant(resize_to_limit: [600, 600]).processed,
                                     only_path: true) if object.image_for_cover.attached?
-    asset_path('images/sidebar-g-1.png')
+    ActionController::Base.helpers.asset_path('images/sidebar-g-1.png')
   end
 end
