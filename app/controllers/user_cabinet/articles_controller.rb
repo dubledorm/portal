81ee5ad1caf50d46ade85d5b0c406d299894501c
Aleksey Aleshin <dubledorm@yandex.ru>
2020-05-04 20:36:56 +0300

    articles_controller вынесен в user_cabinet

--- app/controllers/user_cabinet/articles_controller.rb
+++ app/controllers/user_cabinet/articles_controller.rb
@@ -2,14 +2,6 @@
 module UserCabinet
   class ArticlesController < PrivateAreaController
 
-    has_scope :by_article_type, as: :article_type
-    has_scope :by_state, as: :state
-    has_scope :by_user, as: :user_id
-    has_scope :greater_than_min_age, as: :min_age
-    has_scope :less_than_max_age, as: :max_age
-    has_scope :greater_than_min_quantity, as: :min_quantity
-    has_scope :less_than_max_quantity, as: :max_quantity
-
     def new
       super do
         @user = User.find(params.required(:user_id))
