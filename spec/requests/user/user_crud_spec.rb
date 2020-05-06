1ee40cb9e50a66ae79e2078e09bd2230e800ce28
Aleksey Aleshin <dubledorm@yandex.ru>
2020-05-06 12:37:05 +0300

    тесты для user_cabinet

--- spec/requests/user/user_crud_spec.rb
+++ spec/requests/user/user_crud_spec.rb
@@ -16,12 +16,41 @@ RSpec.describe 'User', type: :request do
       end
 
       it 'should return record' do
-        get(user_path(@user))
+        get(user_cabinet_path)
         expect(response).to have_http_status(200)
       end
+    end
+  end
 
-      it_should_behave_like 'get response 403' do
-        subject { get(user_path(user1)) }
+
+  describe 'update#' do
+    include DeviseSupport
+
+    before :each do
+      sign_in_user
+    end
+
+    context 'when update main_description and user_parameter does not exist' do
+      subject { put(user_cabinet_path(user: { main_description: 'Какое-то значение'})) }
+
+      it { expect{ subject }.to change(UserParameter, :count).by(1) }
+
+      it 'should set main_description' do
+        subject
+        expect(@user.main_description).to eq('Какое-то значение')
+      end
+    end
+
+    context 'when update main_description and user_parameter exists' do
+      before :each do
+        @user.create_user_parameter(description: 'Другое значение')
+      end
+      subject { put(user_cabinet_path(user: { main_description: 'Какое-то значение'})) }
+
+      it { expect{ subject }.to change(UserParameter, :count).by(0) }
+      it 'should set main_description' do
+        subject
+        expect(@user.main_description).to eq('Какое-то значение')
       end
     end
   end
